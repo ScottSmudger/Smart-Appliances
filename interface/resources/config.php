7d656ff49a8b5b295280facce205328898f63180
ScottSmudger <scottsmudger@hotmail.com>
2016-12-30 13:20:15 +0000

    Initial Commit
    Basic files and folder structure

---
+++ interface/resources/config.php
@@ -0,0 +1,18 @@
+<?php
+
+// Database details
+$CONFIG["db_host"] = "localhost";
+$CONFIG["db_user"] = "";
+$CONFIG["db_password"] = "";
+$CONFIG["db_database"] = "";
+
+
+
+
+
+
+// Enable error reporting for all levels
+ini_set("error_reporting", True);
+error_reporting(E_ALL);
+
+?>

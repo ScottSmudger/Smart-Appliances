686e4dfba0954dde4c59b6f88dc502c2bd84d7c8
ScottSmudger <scottsmudger@hotmail.com>
2017-01-01 17:12:26 +0000

    Data from database
    Created the database class and implemented the functions

--- interface/index.php
+++ interface/index.php
@@ -42,11 +42,6 @@ require_once("resources/init.php");
 	</head>
 <body>
 	<?php
-	echo"config array:<pre>";
-	var_dump($CONFIG);
-	?>
-	</pre>
-	<?php
 		require_once("resources/header.php");
 	?>
 	<main>
@@ -54,10 +49,27 @@ require_once("resources/init.php");
 			<div class="row">
 			  <div class="col-sm-4">
 				<div id="topleft">
-					<h3>User Details</h3>
-					<h4>Name</h4><p>Name</p>
-					<h4>Age</h4><p>Age</p>
-					<h4>Address</h4><p>Address</p>
+					<table class="table-striped">
+						<thead>
+							<tr class="theadings">
+								<th>Name</th>
+								<th>Address</th>
+								<th>Emergency Contact Name</th>
+								<th>Emergency Contact Details</th>
+							</tr>
+						</thead>
+						</tbody><?php
+							foreach($database->getUsersInfo() as $user)
+							{
+								echo"<tr>";
+									echo"<td>".ucfirst(strtolower($user["FIRST_NAME"]))." ".ucfirst(strtolower($user["LAST_NAME"]))."</td>";
+									echo"<td>".ucwords(strtolower($user["HOUSE_NO_NAME"].", ".$user["STREET"].", ".$user["TOWN_CITY"]))."<br/>".$user["POSTCODE"]."</td>";
+									echo"<td>".ucfirst(strtolower($user["G_FIRST_NAME"]))." ".ucfirst(strtolower($user["G_LAST_NAME"]))."</td>";
+									echo"<td>".strtolower($user["EMAIL"])."<br/>".$user["PHONE"]."</td>";
+								echo"</tr>";
+							} ?>
+						</tbody>
+					</table><br/>
 				</div>
 				<div id="bottomleft">
 					<h3>View Activity</h3></br>
@@ -73,35 +85,28 @@ require_once("resources/init.php");
 			  </div>
 			  <div class="col-sm-8">
 				<h2>User Data</h2>
-				<table>
-					<tr class="theadings">
-						<th>ID</th>
-						<th>User ID</th>
-						<th>State</th>
-						<th>Date/Time</th>
-						<th>Appliance</th>
-					</tr>
-					<tr class="set1">
-						<td> ****** </td>
-						<td> ****** </td>
-						<td> ****** </td>
-						<td> ****** </td>
-						<td> ****** </td>
-					</tr>
-					<tr class="set2">
-						<td> ****** </td>
-						<td> ****** </td>
-						<td> ****** </td>
-						<td> ****** </td>
-						<td> ****** </td>					
-					</tr>
-					<tr class="set1">
-						<td> ****** </td>
-						<td> ****** </td>
-						<td> ****** </td>
-						<td> ****** </td>
-						<td> ****** </td>
-					</tr>
+				<table class="table-striped">
+					<thead>
+						<tr class="theadings">
+							<th>Device ID</th>
+							<th>User ID</th>
+							<th>State</th>
+							<th>Date/Time Updated</th>
+							<th>Appliance</th>
+						</tr>
+					</thead>
+					</tbody><?php
+						foreach($database->getAllDevicesState() as $device)
+						{
+							echo"<tr>";
+								echo"<td>".$device["device_id"]."</td>";
+								echo"<td>".$device["user_id"]."</td>";
+								echo"<td>".$device["state"]."</td>";
+								echo"<td>".$device["date_time"]."</td>";
+								echo"<td>".$device["appliance"]."</td>";
+							echo"</tr>";
+						} ?>
+					</tbody>
 				</table>
 				<img id="chart" src="/resources/images/chart.png">
 			  </div>
--- interface/resources/footer.php
+++ interface/resources/footer.php
@@ -1,3 +1,3 @@
-<footer>
+<footer style="height: 15px">
 	<h5><i>Copyright&copy; Group 11</i></h5>
 </footer>

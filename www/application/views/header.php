d65c11ec5f14b0cdec11fa769d65131a6820285f
ScottSmudger <scottsmudger@hotmail.com>
2017-02-13 18:12:43 +0100

    Temp Graph

--- www/application/views/header.php
+++ www/application/views/header.php
@@ -25,6 +25,8 @@
 	<meta name="theme-color" content="#ffffff">
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="//code.highcharts.com/highcharts.js"></script>
+	<script src="//code.highcharts.com/modules/exporting.js"></script>
 </head>
 <body>
 <header>
--- www/application/views/pages/dash.php
+++ www/application/views/pages/dash.php
@@ -12,9 +12,11 @@
 				<h3 class="title">View Activity</h3><br/>
 				<h4 class="subtitle">Devices</h4>
 				<select id="dropdown1" name="Device">
-					<option value="" ></option>
-					<option value="Fridge">Fridge</option>
-					<option value="Microwave">Microwave</option>
+					<option value="0">All Devices</option><?php
+						foreach($user->devices as $device)
+						{
+							echo "<option value=".$device->id.">".$device->appliance."</option>";
+						} ?>
 				</select><br/><br/>
 				<button id="enter" type="button" onclick="">Enter</button>
 				<br/><br/><br/><br/><br/><br/>
@@ -46,7 +48,73 @@
 				</table>
 				</br>
 			</div>
-			<img id="chart" src="/application/assets/images/chart.png">
+			<div id="container"></div>
 		</div>
 	</div>
 </div>
+
+<script>
+Highcharts.chart('container', {
+	title: {
+		text: 'State of Devices from '
+	},
+
+	xAxis: {
+		title: {
+			text: 'Time'
+		}
+	},
+
+	yAxis: {
+		title: {
+			text: 'State'
+		}
+	},
+
+	legend: {
+		layout: 'vertical',
+		align: 'right',
+		verticalAlign: 'middle'
+	},
+
+	plotOptions: {
+		series: {
+			pointStart: 2017
+		}
+	},
+
+	series: [{
+		name: 'Fridge',
+		data: [
+			[1486972800, 0],
+			[1486980000, 0],
+			[1486983600, 1],
+			[1486985400, 0],
+			[1486987200, 1],
+			[1486989000, 0],
+			[1486999800, 1],
+			[1487001600, 0],
+			[1487001600, 0],
+			[1487005200, 0],
+			[1487008800, 1],
+			[1487016000, 1]
+		]
+	}, {
+		name: 'Microwave',
+		data: [
+			[1486972800, 1],
+			[1486980000, 0],
+			[1486983600, 0],
+			[1486985400, 1],
+			[1486987200, 0],
+			[1486989000, 1],
+			[1486999800, 0],
+			[1487001600, 1],
+			[1487001600, 1],
+			[1487005200, 1],
+			[1487008800, 0],
+			[1487016000, 0]
+		]
+	}]
+});
+</script>

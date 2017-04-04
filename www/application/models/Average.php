8ae9cbe44df4528c1741911e5b5671cc89c97f91
ScottSmudger <scottsmudger@hotmail.com>
2017-04-04 16:36:34 +0200

    Error handling/avoiding

--- www/application/models/Average.php
+++ www/application/models/Average.php
@@ -21,72 +21,78 @@ class Average extends CI_Model
 	*/
 	public function calculate()
 	{
-		$min = strtotime("-1 week");
+		$min = strtotime("-3 week");
 		$max = strtotime("tomorrow", time()) - 1;
-
-		$this->db->select("state, date_time");
-		$this->db->from("DEVICE_HISTORY");
-		$this->db->where("state", "1");
-		$this->db->where("date_time BETWEEN $min AND $max");
-		$this->result = $this->db->get();
 		
-		return $this->getAvg();
+		return $this->calcAvg($min, $max);
 	}
 	
 	/**
 	* API - For the API as we need to define an arbitrary period of time
 	*
-	* @return $this->getAvg() Array of averages
+	* @return array $this->getAvg($result) Array of averages
 	*/
 	public function api()
 	{
 		$min = strtotime("-1 month");
 		$max = strtotime("tomorrow", time()) - 1;
-
-		$this->db->select("state, date_time");
-		$this->db->from("DEVICE_HISTORY");
-		$this->db->where("state", "1");
-		$this->db->where("date_time BETWEEN $min AND $max");
-		$this->result = $this->db->get();
 		
-		return $this->getAvg();
+		return $this->calcAvg($min, $max);
 	}
 	
 	/**
 	* getAvg - Starts calculating all of the averages
 	*
-	* @return $this->getAvg() Array of averages
+	* @return $averages array Array of averages
 	*/
-	protected function getAvg()
+	protected function calcAvg($min, $max)
 	{
-		// Separates times into hourly periods
-		$counter = 0;
-		foreach($this->result->result_array() as $row)
+		if($min AND $max)
 		{
-			settype($row["date_time"], "int");
-			
-			$hour = date("H", $row["date_time"]);
-			settype($hour, "int");
-			
-			$data[$hour]["times"][] = $row["date_time"];
-		}
-		
-		// Calculates the averages for each hourly period
-		foreach($data as $hour)
-		{
-			// After each loop of the hour we need to reset the total
-			// in order to calculate the new average
-			$total = 0;
-			foreach($hour["times"] as $time)
+			// Get the data
+			$this->db->select("state, date_time");
+			$this->db->from("DEVICE_HISTORY");
+			$this->db->where("state", "1");
+			$this->db->where("date_time BETWEEN ".$min." AND ".$max."");
+			$result = $this->db->get();
+
+			// If there are results
+			if($result AND $result->num_rows() > 0)
 			{
-				$total = $total + $time;
+				// Separates times into hourly periods
+				$counter = 0;
+				foreach($result->result_array() as $row)
+				{
+					settype($row["date_time"], "int");
+					
+					$hour = date("H", $row["date_time"]);
+					settype($hour, "int");
+					
+					$data[$hour]["times"][] = $row["date_time"];
+				}
+				
+				// Calculates the averages for each hourly period
+				foreach($data as $hour)
+				{
+					// After each loop of the hour we need to reset the total
+					// in order to calculate the new average
+					$total = 0;
+					foreach($hour["times"] as $time)
+					{
+						$total = $total + $time;
+					}
+					
+					$count = count($hour["times"]);
+					
+					$averages[] = $total / $count;
+				}
+				
+				return $averages;
 			}
-			
-			$count = count($hour["times"]);
-			
-			$averages[] = $total / $count;
 		}
-		
-		return $averages;
+		else
+		{
+			show_error("No min or max variables set", 500, "Could not calculate averages");
+		}
 	}
 }
